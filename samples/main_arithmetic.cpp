8ec5af22cc5ed7d064691e106f4ea1b60ad5a1aa
Arseniy-1-1-1 <yusipov2006@gmail.com>
2025-12-09 20:40:43 +0300

    Laba i otchet

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,29 @@
 // реализация пользовательского приложения
+#include <iostream>
+#include <string>
+#include "arithmetic.h"
 
-int main()
-{
-  return 0;
+int main() {
+    std::cout << "Vvedite arithm virazhenie: ";
+    std::string expr;
+    std::getline(std::cin, expr);
+
+    try {
+        // Преобразование в ОПЗ
+        auto rpn = toRPN(expr);
+
+        std::cout << "\nOPZ: ";
+        for (const auto& token : rpn)
+            std::cout << token << " ";
+
+        // Вычисление
+        double result = evalRPN(rpn);
+
+        std::cout << "\nRes: " << result << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "\nError: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
